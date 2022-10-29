9bc0ec1cdf8008325a9f30208a7a7c3a2b9992cd
David Sadler <djs21@ic.ac.uk>
2022-10-29 02:06:28 +0100

    Added particle_init function

--- src/particle.c
+++ src/particle.c
@@ -1,5 +1,17 @@
 #include "particle.h"
 
+struct particle particle_init(double mass, double x, double y) {
+    struct particle particle;
+    particle.mass = mass;
+    particle.position.x = x;
+    particle.position.y = y;
+    particle.velocity.x = 0;
+    particle.velocity.y = 0;
+    particle.force.x = 0;
+    particle.force.y = 0;
+    return particle;
+}
+
 void particle_apply_force(struct particle *particle, struct vec2 force) {
     add_to(&particle->force, force);
 }
